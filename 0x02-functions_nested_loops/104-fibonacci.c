f0d22720478581406f6338b2d5e6727472f9982f
HarotAsrat <harotasrat21@gmail.com>
2022-04-16 14:08:49 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -40,7 +40,7 @@ printf("%lu%lu", half1, half2);
 if (count != 98)
 	printf(", ");
 fib1_half1 = fib2_half1;
-fib1_half2 = fib2_half;
+fib1_half2 = fib2_half2;
 fib2_half1 = half1;
 fib2_half2 = half2;
 }
