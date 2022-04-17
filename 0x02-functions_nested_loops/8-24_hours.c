26d53317d4240d811ebdf54f4c5e0b385fa9a11b
HarotAsrat <harotasrat21@gmail.com>
2022-04-17 12:09:43 -0700

    nested function loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - Prints every minute of the day of 
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
@@ -17,7 +18,6 @@ _putchar(':');
 _putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
 _putchar('\n');
-return (0);
 }
 }
 }
