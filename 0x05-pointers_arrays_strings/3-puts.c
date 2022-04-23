da46234f3ece8ac1e1e316ebb3ab7403633aa2c8
HarotAsrat <harotasrat21@gmail.com>
2022-04-23 02:31:47 -0700

    task 0-1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: A pointer to an int that will be changed
+ *
+ * Retrun: void which means our answer is correct
+ */
+
+void _puts(char *str)
+{
+
+char **c'
+int _
+  = 
+}	
